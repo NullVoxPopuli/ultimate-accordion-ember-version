422adb55c905a440974533d9b5acad4b919a2a10
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-09-30 18:15:42 -0400

    linting

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'quotes': 'single',
+    'no-inline-styles': false,
+    'no-invalid-interactive': {
+      additionalInteractiveTags: [ 'a' ]
+    },
+    'attribute-indentation': false
+  }
 };
